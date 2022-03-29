0344cc50e8ff95b1ce50a688b2cd365815e8d456
ChrisH <98359604+n8work@users.noreply.github.com>
2022-03-29 17:17:08 +0200

    added router again

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require("express");
 const app = express();
 const port = 8000;
 
+let router = express.Router();
+
 require("dotenv").config();
 
 var pg = require("pg");
@@ -10,7 +12,7 @@ var conString = process.env.ELEPHANT_SQL_URL;
 var client = new pg.Client(conString);
 client.connect(function (err) {
   if (err) {
-    return console.error('could not connect to postgres', err);
+    return console.error("could not connect to postgres", err);
   }
 });
 
@@ -28,17 +30,34 @@ function getRecipe(slug) {
         return console.error("error running query", err);
       }
 
-      console.log(JSON.stringify(result.rows[0], null, 2));
+      // console.log(JSON.stringify(result.rows[0], null, 2));
       client.end();
     }
   );
 }
 
-app.get("/recipe/:id", (req, res) => {
+app
+  .route("/")
+  .get((req, res) => {
+    res.send("root");
+    console.log("----> Connected to root '/' route");
+  })
+  .post((req, res) => {
+    res.send("Add a");
+  })
+  .put((req, res) => {
+    res.send("Update");
+  });
+
+app.listen(port, () => {
+  console.log(`Express Server on port ${port}`);
+});
+
+/* app.get("/recipe/:id", (req, res) => {
   const recipeId = req.params.id;
   res.send("Recipe " + getRecipe(recipeId));
 });
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
-});
+}); */
