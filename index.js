0cdcdf15342a2c0ee3a32d75c6bd16892df39d1f
Olin Kirkland <me@olinkirk.land>
2022-03-29 17:07:23 +0200

    Move src folder

--- index.js
+++ index.js
@@ -2,32 +2,38 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+require('dotenv').config();
+
+var pg = require('pg');
+
+var conString = process.env.ELEPHANT_SQL_URL;
+var client = new pg.Client(conString);
+
+function getRecipe(slug) {
+  client.connect(function (err) {
+    if (err) {
+      return console.error('could not connect to postgres', err);
+    }
+  });
+
+  client.query(
+    `SELECT * FROM recipe WHERE slug = ${slug}`,
+    function (err, result) {
+      if (err) {
+        return console.error('error running query', err);
+      }
+
+      console.log(JSON.stringify(result.rows[0], null, 2));
+      client.end();
+    }
+  );
+}
+
 app.get('/recipe/:id', (req, res) => {
   const recipeId = req.params.id;
-  res.send('Recipe ' + recipeId);
+  res.send('Recipe ' + getRecipe(recipeId));
 });
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
 });
-
-// require('dotenv').config();
-
-// var pg = require('pg');
-
-// var conString = process.env.ELEPHANT_SQL_URL;
-// var client = new pg.Client(conString);
-
-// client.connect(function (err) {
-//   if (err) {
-//     return console.error('could not connect to postgres', err);
-//   }
-
-//   client.query('SELECT * FROM recipe WHERE id = 1', function (err, result) {
-//     if (err) {
-//       return console.error('error running query', err);
-//     }
-//     console.log(JSON.stringify(result.rows[0], null, 2));
-//     client.end();
-//   });
-// });
